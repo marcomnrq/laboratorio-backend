c651562052afda54f329b3f51d54cbe9ac9529ee
marcomnrq <63822742+marcomnrq@users.noreply.github.com>
2025-09-15 22:14:50 -0500

    Esto deberia fallar

--- scoring.py
+++ scoring.py
@@ -5,7 +5,7 @@ def evaluar_cliente(edad: int, ingresos: float) -> dict:
     if ingresos < 5000:
         return {"aprobado": True, "categoria": "Tarjeta OH", "linea": 2000}
 
-    if ingresos >= 5000 and edad >= 25:
+    if ingresos >= 50000 and edad >= 25:
         return {"aprobado": True, "categoria": "Tarjeta OH Premium", "linea": 10000}
 
     return {"aprobado": True, "categoria": "Tarjeta OH", "linea": 2000}
